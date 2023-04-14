a25895d7d3f1f588a3e6a585008cc217e4e36137
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-04-14 22:42:53 +0530

    Update Main.java

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -339,6 +339,46 @@ public class Main {
     }
 
 
+   /* Given a number N. Your task is to check whether it is fascinating or not.
+    Fascinating Number: When a number(should contain 3 digits or more) is multiplied by 2 and 3,
+     and when both these products are concatenated with the original number,
+     then it results in all digits from 1 to 9 present exactly once.
+
+     Example 1:
+
+    Input:
+    N = 192
+    Output: Fascinating
+    Explanation: After multiplication with 2
+    and 3, and concatenating with original
+    number, number will become 192384576
+    which contains all digits from 1 to 9.
+
+    Example 2:
+
+    Input:
+    N = 853
+    Output: Not Fascinating
+    Explanation: It's not a fascinating
+    number.
+    Your Task:
+    You don't need to read input or print anything. Your task is to complete the function fascinating()
+     which takes the integer n parameters and returns boolean(True or False) denoting the answer.
+*/
+
+   public static boolean fascinating(long num) {
+       // answer seen
+        if(num>=0 && num<=99) return false;
+        long pluse3 = num*3;
+        long pluse2 =num*2;
+        String s = num+ ""+pluse2+""+pluse3;
+        int arr[] = new int [10];
+        for(int i=0; i<9; i++) arr[i] =s.charAt(i)-48;
+        Arrays.sort(arr);
+        for(int i=0; i<9; i++) if(arr[i] !=i) return false;
+        return true;
+
+    }
 
 
 
@@ -422,6 +462,9 @@ public class Main {
         long[] arrLong = {1,5,2,3,7,9,6,4};
         long lenArr = 8;
         leftElement(arrLong,lenArr);
+
+        // fascinating number
+        fascinating(lenArr);
 
     }
 }
