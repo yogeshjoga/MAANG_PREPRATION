1ac411f86f993a258a81cf9df107dc9f60eb7550
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-04-14 10:26:06 +0530

    left side element printing
    left side element printing

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -307,6 +307,37 @@ public class Main {
         return String.format("%.2f",temp/len);
     }
 
+    // Print the left element
+   /* Given a array of length N, at each step it is reduced by 1 element. In the first step the maximum element would be removed,
+    while in the second step minimum element of the remaining array would be removed,
+   in the third step again the maximum and so on. Continue this till the array contains only 1 element.
+    And find the final element remaining in the array.
+
+    Example 1:
+    Input:
+    N = 7
+    A[] = {7, 8, 3, 4, 2, 9, 5}
+    Ouput:
+            5
+    Explanation:
+    In first step '9' would be removed, in 2nd step
+    '2' will be removed, in third step '8' will be
+    removed and so on. So the last remaining
+    element would be '5'.
+
+    Example 2:
+    Input:
+    N = 8
+    A[] = {8, 1, 2, 9, 4, 3, 7, 5}
+    Ouput:
+            4*/
+    public static long leftElement(long[] arr, long len){
+        Arrays.sort(arr);
+        int temp = arr.length;
+        // print left element
+        return (len%2==0)?(arr[temp/2-1]):(arr[temp/2]);
+    }
+
 
 
 
@@ -385,6 +416,12 @@ public class Main {
 
         // array set
         sumArrDiv(arr, len);
+
+
+        // print Left Element
+        long[] arrLong = {1,5,2,3,7,9,6,4};
+        long lenArr = 8;
+        leftElement(arrLong,lenArr);
 
     }
 }
