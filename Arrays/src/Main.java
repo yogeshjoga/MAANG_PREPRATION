08448f3dc862c727ee0f3e9782d491cdc6849307
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-04-15 23:52:22 +0530

    Arrays completed

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -420,6 +420,66 @@ public class Main {
     }
 
 
+    //
+
+   /* A and B are good friend and programmers. They are always in a healthy competition with each other.
+    They decide to judge the best among them by competing. They do so by comparing their three skills as per their values.
+    Please help them doing so as they are busy.Set for A are like [a1, a2, a3]
+    Set for B are like [b1, b2, b3]
+
+    Compare ith skill of A with ith skill of B
+            if a[i] > b[i] , A's score increases by 1
+            if a[i] < b[i] , B's score increases by 1
+            if a[i] = b[i] , nothing happens
+
+    Example 1:
+
+    Input :
+    A = {4, 2, 7}
+    B = {5, 6, 3}
+    Output :
+            1 2
+
+    Example 2:
+
+    Input :
+    A = {4, 2, 7}
+    B = {5, 2, 8}
+    Output :
+            0 2
+
+    Your Task:
+    You don't need to read input or print anything. Your task is to complete the function scores()
+    which takes the array A[], B[] as inputs and two reference variables ca and cb to store the final answer.
+    Store the score of A in ca and score of B in cb.*/
+
+
+    public static void scores(long a[], long b[]) {
+        // store the answer as
+        // GFG.ca=3;
+        // GFG.cb=5;
+        // Your code goes here
+
+     int [] res = new int[2];
+
+      // GFG.ca=0;  this is for GFG code input variables
+      //  GFG.cb=0;
+       int temp =0;
+       int temp1= 0;
+
+        for(int i=0; i<a.length; i++){
+            if(a[i] > b[i]){
+                temp++;
+              //  GFG.ca++;
+            }if(a[i] < b[i]){
+                temp1++;
+             //   GFG.cb++;
+            }
+            res[0]= temp;
+            res[1]= temp1;
+
+        }
+    }
 
 
 
@@ -511,6 +571,14 @@ public class Main {
 
         // Average in a stream
         avgInStream( arr,  len);
+
+        // 22 problem
+        // arrays completed
+        long[] yogi ={1,2,3,4};
+        long[] balu ={11,2,31,4};
+        scores( yogi,  balu);
+
+
 
     }
 }
