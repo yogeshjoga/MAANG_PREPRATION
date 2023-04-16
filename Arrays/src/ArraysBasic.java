0162403c75e60d5a87c122cfde45b45ebdb914e5
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-04-16 23:58:31 +0530

    Update ArraysBasic.java

--- Arrays/src/ArraysBasic.java
+++ Arrays/src/ArraysBasic.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ArraysBasic {
 
@@ -111,6 +113,53 @@ public class ArraysBasic {
 
 
 
+    //3rd question Reverse array in groups
+  /*  Given an array arr[] of positive integers of size N. Reverse every sub-array group of size K.
+
+            Note: If at any instance, there are no more subarrays of size greater than or equal to K,
+             then reverse the last subarray (irrespective of its size). You shouldn't return any array,
+             modify the given array in-place.
+
+    Example 1:
+
+    Input:
+    N = 5, K = 3
+    arr[] = {1,2,3,4,5}
+    Output: 3 2 1 5 4
+    Explanation: First group consists of elements
+1, 2, 3. Second group consists of 4,5.
+
+    Example 2:
+
+    Input:
+    N = 4, K = 3
+    arr[] = {5,6,8,9}
+    Output: 8 6 5 9
+
+
+    Your Task:
+    You don't need to read input or print anything. The task is to complete the function reverseInGroups()
+    which takes the array, N and K as input parameters and modifies the array in-place. */
+
+    void reverseInGroups(ArrayList<Integer> arr, int n, int k) {
+        int len =0;
+        int m = Math.min(n,k);
+        while(len<=m){
+            Collections.swap(arr,m-1,len);
+            len+=k;
+            m = Math.min((m+k),n);
+        }
+    }
+   // correct solution
+   void reverseInGroups2(ArrayList<Integer> arr, int n, int k) {
+     for(int i=0;i<n;i=i+k) {
+         int left=i;
+         int right=Math.min(n-1,k+i-1);
+         while(left<right) {
+             Collections.swap(arr,left++,right--);
+         }
+     }
+  }
 
 
     public static void main(String[] args) {
@@ -124,11 +173,13 @@ public class ArraysBasic {
         // recursion
         binarySearchIndex2(arr, len, key);
 
-        //
+        // Array equals
         long[] A = {1,3,2,7,4,6,5};
         long[] B = {2,1,4,3,6,5,7};
         int N = 7;
         check(A,B,N);
+
+        // 3rd solution
 
     }
 }
