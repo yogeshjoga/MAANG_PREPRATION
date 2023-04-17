fcc9d7501400e75360d4f0c0f891865953fcc91f
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-04-17 14:50:06 +0530

    java basics

---
+++ Revision_JAVA_Basics/src/RevisionJavaBasic.java
@@ -0,0 +1,11 @@
+public class RevisionJavaBasic {
+    /**
+     * BrushUp java Basics
+     * Daily learn one topic
+     *
+     * @Author YogeshJoga
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
