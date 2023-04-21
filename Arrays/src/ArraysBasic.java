7eace06610043e74b19a37a08d91eadf7a2991dc
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-04-21 12:54:21 +0530

    ZigZag array conversion

--- Arrays/src/ArraysBasic.java
+++ Arrays/src/ArraysBasic.java
@@ -284,7 +284,7 @@ public class ArraysBasic {
 
     public static long[] getMinMax(long arr[], long len){
         /*  pair class object return type*/
-            long[] getmm = new long[1];
+            long[] getmm = new long[2];
             int arrLen = arr.length;
             Arrays.sort(arr);
             long min = arr[0];
@@ -377,6 +377,52 @@ public class ArraysBasic {
     }
 
 
+    // Convert array into Zig-Zag fashion
+    /*Given an array arr of distinct elements of size N,
+    the task is to rearrange the elements of the array in a zig-zag fashion so that the converted array should be in the below form:
+
+    arr[0] < arr[1]  > arr[2] < arr[3] > arr[4] < . . . . arr[n-2] < arr[n-1] > arr[n].
+
+    NOTE: If your transformation is correct, the output will be 1 else the output will be 0.
+
+    Example 1:
+
+    Input:
+    N = 7
+    Arr[] = {4, 3, 7, 8, 6, 2, 1}
+    Output: 3 7 4 8 2 6 1
+    Explanation: 3 < 7 > 4 < 8 > 2 < 6 > 1
+    Example 2:
+
+    Input:
+    N = 4
+    Arr[] = {1, 4, 3, 2}
+    Output: 1 4 2 3
+    Explanation: 1 < 4 > 2 < 3*/
+    public static void zigZagArray(int[] arr, int len){
+        int temp =0;
+        for(int i=0; i<len-1; i++){
+            if(i % 2 == 0){
+                if(arr[i] > arr[i+1]){
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }else{
+                    if(arr[i] < arr[i+1]){
+                        temp = arr[i];
+                        arr[i] = arr[i+1];
+                        arr[i+1] = temp;
+                    }
+                }
+            }
+        }
+        for(int ll:arr){
+            System.out.println(ll);
+        }
+    }
+
+    //
+
 
 
 
@@ -438,6 +484,11 @@ public class ArraysBasic {
         int lenHa = 10;
         int keyH =2;
         firstElementKTime(arrHa,lenHa, keyH);
+
+        //zigZagArray
+        int[] arrZig ={4,3,7,1,0,2,1};
+        int lenZig = 7;
+        zigZagArray(arrZig,lenZig);
 
 
     }
