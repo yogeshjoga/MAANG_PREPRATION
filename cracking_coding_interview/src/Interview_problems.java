ec4f4a26fd637b9de2b7981aee665576d0f75635
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-04-22 23:37:56 +0530

    Started Cracking coding interview
    this book gave enough knowledge to our interview prepartion

---
+++ cracking_coding_interview/src/Interview_problems.java
@@ -0,0 +1,13 @@
+public class Interview_problems {
+    /**
+     *
+     * @param args
+     */
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
