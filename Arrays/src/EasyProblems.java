481bd238a96acf88fd754dd55bc3cbba511d73d5
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-04-25 22:55:24 +0530

    stock buy and sell

--- Arrays/src/EasyProblems.java
+++ Arrays/src/EasyProblems.java
@@ -305,6 +305,66 @@ public class EasyProblems {
 
     }
 
+    // 6th problem Stock buy and sell
+    /*The cost of stock on each day is given in an array A[] of size N. Find all the segments of days on which you buy
+    and sell the stock so that in between those days  for which profit can be generated
+
+            Note: Since there can be multiple solutions, the driver code will print 1 if your answer is correct,
+            otherwise, it will return 0. In case there's no profit the driver code will print the string "No Profit" for a correct solution.
+
+    Example 1:
+
+    Input:
+    N = 7
+    A[] = {100,180,260,310,40,535,695}
+    Output:
+            1
+    Explanation:
+    One possible solution is (0 3) (4 6)
+    We can buy stock on day 0,
+    and sell it on 3rd day, which will
+    give us maximum profit. Now, we buy
+    stock on day 4 and sell it on day 6.
+    Example 2:
+
+    Input:
+    N = 5
+    A[] = {4,2,2,2,4}
+    Output:
+            1
+    Explanation:
+    There are multiple possible solutions.
+    one of them is (3 4)
+    We can buy stock on day 3,
+    and sell it on 4th day, which will
+    give us maximum profit.*/
+
+    public static ArrayList<ArrayList<Integer>> stockBuyAndSell(int[] arr, int len){
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        int buy = 0;
+        int sell = 0;
+        for(int i=0; i<len-1; i++){
+            if(arr[buy] < arr[i+1]) {
+                sell++;
+            }else{
+                if(buy < sell){
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(buy);
+                list.add(sell);
+                result.add(list);
+                }
+               buy = i+1;
+                sell = i+1;
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(buy);
+        list.add(sell);
+        result.add(list);
+        return result;
+    }
+
+
 
 
 
