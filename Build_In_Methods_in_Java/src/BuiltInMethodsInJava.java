855bfc1c919e676d2e1327736b8e54d7390ffcf4
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-04-30 17:32:49 +0530

    METHODS

--- Build_In_Methods_in_Java/src/BuiltInMethodsInJava.java
+++ Build_In_Methods_in_Java/src/BuiltInMethodsInJava.java
@@ -1,5 +1,24 @@
+import java.time.Clock;
+
 public class BuiltInMethodsInJava {
+    /**
+     *
+     * @param args
+     */
+
+    // Basic math Class Methods
+    public static void absMethod(){
+        //
+
+    }
+
+
+
     public static void main(String[] args) {
+        Math.max(10,50);
+        double result = Math.fma(10,20,2);
+        System.out.println(result);
+
         System.out.println("Hello world!");
     }
 }
