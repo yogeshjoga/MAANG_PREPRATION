2c456fd189d5059f238e206f8697c7e0dfad112c
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-05-04 23:24:04 +0530

    1st medium prolbem

--- Arrays/src/MediumArrays.java
+++ Arrays/src/MediumArrays.java
@@ -7,4 +7,36 @@ public class MediumArrays {
      *  I don't have enough knowledge today but end of this class implementation i need to improve my skills.
      */
 
+
+    // 1st medium problem
+    public static long maxSubArraySum(long[] arr, int len){
+        /**
+         * here Integer.MAX_VALUE and Integer.MIN_VALUE
+         * MAX_VALUE --> mean int Highest positive number,  It means size of int is 4Bytes -- 32bit
+         * MIN_VALUE --> mean int Highest negative number   constraints:--> -2^31 to 2^31-1
+         */
+        // int minVal = Integer.MIN_VALUE;
+        long minVal = -99992929;
+        long  sum = 0L;
+        for(int i=0; i<len; i++){
+            sum += arr[i];
+            if (minVal < sum) {
+                minVal = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        System.out.println(minVal);
+        return minVal;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // 1st algo kadane
+        long arr[] ={-8,1,2,3,4,-5,2,-9};
+        int len = 8;
+        maxSubArraySum(arr,len);
+    }
+
 }
