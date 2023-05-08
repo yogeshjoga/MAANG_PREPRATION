c73f7b6afed33398e3c5228cec2bf5fc9a7209f1
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-05-08 23:41:25 +0530

    string school level problem

--- Strings/src/School.java
+++ Strings/src/School.java
@@ -7,10 +7,25 @@ public class School {
      *
      */
 
-    // 1st problem
+    // 1st problem Check for Binary
+    // its working fine
+     public  static boolean binaryCheck(String str){
+         // iterating the string char's
+         for(int i=0; i<str.length(); i++){
+             if(str.charAt(i) != '0' && str.charAt(i) != '1'){
+                 System.out.println(false);
+                 return  false;
+             }
+         }
+         System.out.println(true);
+         return true;
+     }
 
 
     public static void main(String[] args) {
+         // 1st problem
+        String str = "1010101010";
+          binaryCheck(str);
 
     }
 }
