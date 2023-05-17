96a3e12c69eeb08a5afb1e501bf06fb9eae64f1e
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-05-17 22:26:40 +0530

    updated

---
+++ Java8to17/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    /**
+     *
+     * @param args
+     */
+
+    // searching algo's
+    public int search(int a, int b){
+        var c = a+b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+     //   System.out.println(new Main.search(10,20));
+
+        System.out.println("Hello world!");
+        List<Integer> he = new ArrayList<>();
+    }
+}
