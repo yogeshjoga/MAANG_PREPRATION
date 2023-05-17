ecd015bfab6ca75a308ebe49bea0e02af287ca45
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-05-18 01:21:28 +0530

    arrays stirng

--- Strings/src/School.java
+++ Strings/src/School.java
@@ -1,3 +1,5 @@
+import java.awt.color.ICC_ColorSpace;
+
 public class School {
     /**
      *
@@ -111,6 +113,88 @@ public class School {
          return total;
     }
 
+    // VOWELS REPLACE
+    public static String vowelReplace(String s){
+         String rep = s.replaceAll("[aeiouAEIOU]","");
+        System.out.println(rep);
+         return rep;
+    }
+
+    // given string conatians same char's or not
+    public static boolean sameChar(String s){
+         char key = s.charAt(0);
+        for(int i=0; i<s.length(); i++){
+            if(key != s.charAt(i)){
+                return false;
+            }
+        }
+      //  System.out.println();
+        return true;
+    }
+
+    // convert char array to string
+    public static String convertString(char[] s){
+         String conv = new String(s);
+        System.out.println(conv);
+        return conv;
+    }
+
+    // lowercase string to uppercase
+    public static String lowerToUpper(String s){
+         String upper = s.toUpperCase();
+        System.out.println(upper);
+      //  java.lang.Character.is
+        return  upper;
+    }
+
+    // count uppercase, lowercase, digit, special
+  public static  int[] count (String s)
+
+    {
+        int upper =0;
+        int lower = 0;
+        int nums =0;
+        int cha = 0;
+        int[] to = new int[4];
+        for(int i=0; i<s.length(); i++){
+            if(java.lang.Character.isUpperCase(s.charAt(i))){
+                upper++;
+            }else if(java.lang.Character.isLowerCase(s.charAt(i))){
+                lower++;
+            }else if(java.lang.Character.isDigit(s.charAt(i))){
+                nums++;
+            }else if(!Character.isDigit(s.charAt(i))){
+                cha++;
+            }
+            to[0] =upper;
+            to[1] =lower;
+            to[2] =nums;
+            to[3] = cha;
+        }
+        return to;
+    }
+    // 2nd solution
+    public static int[] charsCount(String s){
+         int[] output = new int[4];
+         for(int i=0; i<s.length(); i++){
+             if(Character.isUpperCase(s.charAt(i))) output[0]++;
+             else if(Character.isLowerCase(s.charAt(i))) output[1]++;
+             else if(Character.isDigit(s.charAt(i))) output[2]++;
+             else if(!Character.isDigit(s.charAt(i))) output[3]++;
+         }
+         arrayPrint(output);
+         return output;
+    }
+    
+
+    // Array print method
+    public static void arrayPrint(int[] arr){
+         for(int a:arr){
+             System.out.println(a);
+         }
+    }
+
+
 
 
 
@@ -145,6 +229,26 @@ public class School {
             // uppercase count
             String upperCaseCunt = "HelloBABYsiriDarling";
             uppperCaseCount(upperCaseCunt);
+
+            // vowels replace
+            String vowelsrep = "I love You BaBy";
+            vowelReplace(vowelsrep);
+
+            // is string chars is duplicated or not
+            String dupli = "bbbbb";
+            sameChar(dupli);
+
+            // char array to String
+            char[] charAr = {'a','d','c','d','e'};
+            convertString(charAr);
+
+            // lower case string to upper case string
+            String upperCse ="hello baby i Love you siri";
+            lowerToUpper(upperCse);
+
+            // different chars count
+            String charCountsSt = "ABCD123@#$abcd";
+            charsCount(charCountsSt);
 
         }
 
