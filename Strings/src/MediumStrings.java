e929dacc2edf9304e7772027a75ea255edc57b7e
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-05-23 23:35:46 +0530

    string Roman number to integer
    map Datastructers

--- Strings/src/MediumStrings.java
+++ Strings/src/MediumStrings.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 public class MediumStrings {
 
@@ -258,6 +256,56 @@ public class MediumStrings {
         return false;
     }
 
+    // Romain numbers
+
+    /**
+     *Given a string in roman no format (s)
+     *  your task is to convert it to an integer . Various symbols and their values are given below.
+     * I 1
+     * V 5
+     * X 10
+     * L 50
+     * C 100
+     * D 500
+     * M 1000
+     *
+     * Example 1:
+     *
+     * Input:
+     * s = V
+     * Output: 5
+     * Example 2:
+     *
+     * Input:
+     * s = III
+     * Output: 3
+     *
+     */
+
+    public static  int romToInt(String str){
+        // Adding the all pairs
+        // to get these paris and process
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L', 50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int len = str.length()-1;
+        int res = map.get(str.charAt(len--));
+        for(int first, next; len>=0; len--){
+            first =map.get(str.charAt(len));
+            next = map.get(str.charAt(len+1));
+
+            res += first < next ? (-first) : first;
+        }
+        System.out.println(" Roman to Int : "+str+" : "+res);
+        return  res;
+    }
+
     public static void main(String[] args) {
         //  Parenthesis Checker
         String pran = "(){}{[]";
@@ -295,7 +343,9 @@ public class MediumStrings {
         String rote2 = "azonma";
         isRotated(rote,rote2);
 
-
+        // Roman number to integer
+        String romTo = "IV";
+        romToInt(romTo);
 
     }
 }
