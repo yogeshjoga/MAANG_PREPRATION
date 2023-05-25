d21d7fd766369b4cdfcaa8bbb07f4981ec66e0c0
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-05-26 00:39:41 +0530

    updated

--- LinkedList/src/LinkedListGFG.java
+++ LinkedList/src/LinkedListGFG.java
@@ -2,9 +2,25 @@
 class NodeGFG{
     int head;
     NodeGFG next;
+    NodeGFG(int data){
+        head = data;
+        next = null;
+    }
 
 }
 
 
 public class LinkedListGFG {
+    public static boolean detectLoop(NodeGFG head){
+        NodeGFG slowPointer = head, fastPointer = head;
+
+        while(fastPointer != null && fastPointer.next != null)
+        {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+
+            if(slowPointer == fastPointer) return true;
+        }
+        return false;
+    }
 }
