4edb000bd33bfef53fdd35de64f4f470a147c53e
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-05-27 23:53:53 +0530

    linkedlist

--- LinkedList/src/LinkedListGFG.java
+++ LinkedList/src/LinkedListGFG.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class NodeGFG{
     int head;
@@ -23,4 +26,22 @@ public class LinkedListGFG {
         }
         return false;
     }
+
+
+    int getNthFromLast(Node head, int n)
+    {
+        List<Integer> list = new ArrayList<>();
+        Node temp = head;
+
+        while(temp != null){
+            list.add(temp.data);
+            temp = temp.next;
+        }
+        Collections.reverse(list);
+        if(n <= list.size()){
+            return list.get(n-1);
+        }
+        return -1;
+    }
+}
 }
